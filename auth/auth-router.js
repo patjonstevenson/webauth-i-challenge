8081089279739ef629763ffda9fd9d8430d2382d
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-11 14:57:40 -0800

    finished users-model, users-router, auth-router; set up server.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,4 @@
-const router = require(express).Router();
+const router = require('express').Router();
 const bcrypt = require("bcryptjs");
 
 const Users = require('../users/users-model');
@@ -10,7 +10,7 @@ router.post("/register", async (req, res) => {
     };
 
     try {
-        const saved = Users.add(credentials);
+        const saved = await Users.add(credentials);
         res.status(201).json(saved);
     } catch (error) {
         console.log("Error registering:\n", error);
@@ -30,7 +30,7 @@ router.post("/login", async (req, res) => {
             }
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: "Internal server error authorizing." })
+            res.status(500).json({ message: "Internal server error logging in." })
         }
     }
 });
--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,17 @@
-const router = require(express).Router();
+const router = require('express').Router();
 
-router.get("/users", async (req, res) => {
+const requiresAuth = require("../auth/requires-auth-middleware");
 
+const Users = require('./users-model');
+
+router.get("/", requiresAuth, async (req, res) => {
+    try {
+        const users = await Users.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.log("Error fetching users:\n", error);
+        res.status(500).json("Internal server error");
+    }
 });
 
 module.exports = router;
